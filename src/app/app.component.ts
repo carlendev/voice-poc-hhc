815bdbdde3ebf13fc1e6ce3a11206101616d403c
Yann Carlen <yann-carlen@hotmail.fr>
2018-03-24 16:25:39 +0100

    basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'AlzaHome';
+  sounds = [
+    { key: 'eat', url: 'https://translate.google.com/translate_tts?ie=UTF-8&tl=fr&client=tw-ob&q=Bonjour%20Carlen', description: 'Manger Midi' }
+  ];
+
+  play(event) {
+    const audio = new Audio();
+    const elem =this.sounds.find(e => e.key === event.key);
+    if (elem === undefined) return;
+    audio.src = elem.url; 
+    audio.load();
+    audio.play();
+  }
 }
